5899a6882f876c41bb0095a4c0d3ca813839cd74
ojspp000@naver.com <ojspp000@naver.com>
2025-02-06 00:30:44 +0900

    local

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -6,27 +6,22 @@ export const Faq = () => {
   const [faqs, setFaqs] = useState([]);
 
   useEffect(() => {
-    const fetchFaqs = async () => {
-      try {
-        console.log(response);
-        const response = await fetch('https://csiereserve.store/api/admin/faq/getAll');
-        if (!response.ok) {
-          throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
-        }
-        
-        const result = await response.json();
-        console.log("API 응답 데이터:", result);
-        
-        if (result.message === "FAQ 조회 성공" && Array.isArray(result.data)) {
-          setFaqs(result.data);
-        }
-      } catch (error) {
-        console.error("FAQ 데이터를 가져오는 중 오류 발생: ", error);
+  const fetchFaqs = async () => {
+    try {
+      const response = await fetch('https://csiereserve.store/api/admin/faq/getAll');
+      if (!response.ok) {
+        throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
       }
-    };
+      const result = await response.json();
+      setFaqs(result.data || []);
+    } catch (error) {
+      console.error("FAQ 데이터를 가져오는 중 오류 발생: ", error);
+    }
+  };
 
-    fetchFaqs();
-  }, []);
+  fetchFaqs();
+}, []);
+;
   return (
     <div className="faq-container">
       <NavigationBar title="FAQ"/>
